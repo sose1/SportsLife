a5d680e63b52e7eada743936c92089300f5c9076
Kamil Wąsowski <kamil.wasowski00@gmail.com>
2023-04-17 19:56:16 +0200

    Bump Versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ android {
 dependencies {
 
     implementation ("androidx.core:core-ktx:1.10.0")
+
+    //android
     implementation ("androidx.appcompat:appcompat:1.6.1")
     implementation ("com.google.android.material:material:1.8.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
